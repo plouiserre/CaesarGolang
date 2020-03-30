be48c92202c8301f32da92d593eb0977c55ff005
Pierre-Louis Serré <plouiserre@macbook-pro-de-pierre-louis.home>
2020-03-30 19:55:43 +0200

    mise en place du slice alphabet

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import "fmt"
 
 //TODO
 // 1 - pouvoir parcourir les lettres de chiffrement OK
-// 2 - faire l'alphabet
+// 2 - faire l'alphabet OK
 // 3 - faire la clé
 // 4 - trouver la bonne lettre
 // 5 - Gérer les espaces
 // 6 - Afficher la phrase chiffré
 // 7 - mettre le tout dans des structs
-// 8 - commiter
+
+type letters []string
 
 func main() {
+	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	for _, letter := range letters {
+		fmt.Printf(letter)
+	}
 	text := "je suis le texte a chiffrer"
 	displayLetter(text)
 }
