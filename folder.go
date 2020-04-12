5cdc7bc60ccb6cb3f44dbacbf89a3e5bf5cd28d2
Pierre-Louis Serré <plouiserre@macbook-pro-de-pierre-louis.home>
2020-04-12 14:44:44 +0200

    prepare code to use go routine

--- folder.go
+++ folder.go
@@ -36,37 +36,21 @@ func (f *folder) GetAllFiles(){
 	f.files = files
 }
 
-//TODO structurer cette méthode
 func (f folder)CipherFiles(){
 	
 	for _, fileReading := range f.files{
-		fileName := fileReading.Name()
-		fileToCipher := file{
-			pathReadFile : f.pathRead+"/"+fileName,
-			pathWriteFile : f.pathWrite+"/"+fileName,
-		}
 
-		content := fileToCipher.readFile()
-
-		caesarWorkflow := workflow{
-			text : content,
-			caracter : f.allCaracteres,
-			crypt : f.msgToCipher,
-		}
-
-		sentenceCiphered := caesarWorkflow.transformSentence()
-
-		fmt.Printf("%s chiffre donne \n %s \n", fileName, sentenceCiphered)
-
-		fileToCipher.writeCipherMessage(sentenceCiphered)
-
-		caesarWorkflow.setIcipher(f.msgToDecipher)
-		caesarWorkflow.setText(sentenceCiphered)
-
-		oldSentence := caesarWorkflow.transformSentence()
-
-		fmt.Printf("Voici l'ancienne phrase dechiffre %s \n", oldSentence)
+		f.CipherFile(fileReading)
 	}
+}
+
+func (f folder) CipherFile(fileReading os.FileInfo){
+	workflowEncryption := workflow {
+		fileReading : fileReading,
+		folderCrossing : f,
+	}
+
+	workflowEncryption.ManipulateFile()
 }
 
 func (f folder) ClearOutputDirectory(){
--- workflow.go
+++ workflow.go
@@ -1,78 +1,41 @@
-package main
+package main 
 
-import(	
-	"strings"
+import(
 	"fmt"
 	"os"
 )
 
-type workflow struct {
-	text string
-	caracter caracteres
-	crypt icipher 
-	lettersOfNewWord []rune
+type workflow struct{
+	fileReading os.FileInfo
+	folderCrossing folder
 }
 
-func (w *workflow) setIcipher(cipher icipher){
-	w.crypt = cipher
-}
-
-func(w *workflow) setText(newText string){
-	w.text = newText
-}
-
-func (w *workflow)transformSentence() string {
-	words := strings.Split(w.text, " ")
-	var newSentence string
-	for _, word := range words {
-		newWord := w.transformWord(word)
-		newSentence = newSentence + " " + newWord
-	}
-	return newSentence
-}
-
-//TODO réécrire cette méthode (étape 8)
-func (w *workflow)transformWord(word string) string {
-	w.lettersOfNewWord = []rune{}
-	for _, letter := range word {
-		isUpperCase := false 
-		isNormalLetter := false
-		isPunctuation := w.caracter.IsSpecificCaracters(letter, w.caracter.punctuation)
-		if(isPunctuation == false){
-			isUpperCase =  w.caracter.IsSpecificCaracters(letter, w.caracter.uppercaseAlphabet)
-			if(isUpperCase == false){
-				isNormalLetter = w.caracter.IsSpecificCaracters(letter, w.caracter.alphabet)
-			}
-		}
-		
-		//mettre une étape pour faire un continue si c'est ' et l'ajouter dans le truc a chiffrer
-		//TODO améliorer et le mettre en méthode
-		if(letter == 8217 || letter == 10){
-			w.lettersOfNewWord = append(w.lettersOfNewWord, letter)
-			continue
+//TODO structer cette méthode
+func (w workflow) ManipulateFile(){
+	fileName := w.fileReading.Name()
+		fileToCipher := file{
+			pathReadFile : w.folderCrossing.pathRead+"/"+fileName,
+			pathWriteFile : w.folderCrossing.pathWrite+"/"+fileName,
 		}
 
-		if(isPunctuation == false && isUpperCase == false && isNormalLetter == false){
-			fmt.Printf("Dans le mot %s il y a le caractère %s inconnu donc on arrête tout \n", word, string(letter))
-			os.Exit(1)
+		content := fileToCipher.readFile()
+
+		caesarEncryption := encryption{
+			text : content,
+			caracter : w.folderCrossing.allCaracteres,
+			crypt : w.folderCrossing.msgToCipher,
 		}
 
-		if(isPunctuation == false){
-			if(isUpperCase == false){
-				indexLetter := w.caracter.GetSpecificIndex(letter, w.caracter.alphabet)
-				newIndex :=  w.crypt.GetNewIndex(indexLetter)
-				newLetter := w.caracter.GetSpecificCaracters(newIndex, w.caracter.alphabet)
-				w.lettersOfNewWord = append(w.lettersOfNewWord, newLetter)
-			}else {
-				indexLetter := w.caracter.GetSpecificIndex(letter, w.caracter.uppercaseAlphabet)
-				newIndex :=  w.crypt.GetNewIndex(indexLetter)
-				newLetter := w.caracter.GetSpecificCaracters(newIndex, w.caracter.uppercaseAlphabet)
-				w.lettersOfNewWord = append(w.lettersOfNewWord, newLetter)
-			}
-		} else {
-			w.lettersOfNewWord = append(w.lettersOfNewWord, letter)
-		}
-	}
-	newWord := string(w.lettersOfNewWord)
-	return newWord
+		sentenceCiphered := caesarEncryption.transformSentence()
+
+		fmt.Printf("%s chiffre donne \n %s \n", fileName, sentenceCiphered)
+
+		fileToCipher.writeCipherMessage(sentenceCiphered)
+
+		caesarEncryption.setIcipher(w.folderCrossing.msgToDecipher)
+		caesarEncryption.setText(sentenceCiphered)
+
+		oldSentence := caesarEncryption.transformSentence()
+
+		fmt.Printf("Voici l'ancienne phrase dechiffre %s \n", oldSentence)
 }
